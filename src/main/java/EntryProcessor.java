7360713994c21d58180f7d92ef252a6b9fe29dfc
KonradSto <stolarczyk.konrad@outlook.com>
2019-05-08 23:17:58 +0200

    adds working EntryProcessor along with additional EquationComponents class

--- src/main/java/EntryProcessor.java
+++ src/main/java/EntryProcessor.java
@@ -1,2 +1,34 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EntryProcessor {
+    public static EquationComponents processEntry(String entry) {
+        double argumentA = 0;
+        double argumentB = 0;
+        String operator = "";
+        if (!EntryValidator.isLineValid(entry)) {
+            throw new IllegalArgumentException("Invalid entry");
+        }
+        Pattern p = Pattern.compile("(\\d+(\\.\\d+)?)");
+        Matcher matcher = p.matcher(entry);
+        if (matcher.find()) {
+            argumentA = Double.parseDouble(matcher.group(0));
+        }
+        if (matcher.find()) {
+            argumentB = Double.parseDouble(matcher.group(1));
+        }
+        if (entry.contains("+")) {
+            operator = "+";
+        }
+        if (entry.contains("-")) {
+            operator = "-";
+        }
+        if (entry.contains("*")) {
+            operator = "*";
+        }
+        if (entry.contains("/")) {
+            operator = "/";
+        }
+        return new EquationComponents(argumentA, argumentB, operator);
+    }
 }
--- src/test/java/EntryProcessorTest.java
+++ src/test/java/EntryProcessorTest.java
@@ -5,19 +5,36 @@ import org.junit.jupiter.api.Test;
 class EntryProcessorTest {
 
     @Test
-    void shouldReturnCorrectArgumentsAndOperator(){
+    void shouldReturnCorrectArgumentsAndOperator() {
         //Given
         double argument1 = 9.8;
         double argument2 = 1.3;
         String operator = "+";
-        EquationComponents actualEquationComponents = new EquationComponents();
+        EquationComponents actualEquationComponents;
 
         //When
         actualEquationComponents = EntryProcessor.processEntry("9.8 + 1.3");
 
         //Then
-        assertTrue(argument1 == actualEquationComponents.argumentA);
-        assertTrue(argument2 == actualEquationComponents.argumentB);
-        assertTrue(operator.equals(actualEquationComponents.operator));
+        assertEquals(argument1, actualEquationComponents.getArgumentA());
+        assertEquals(argument2, actualEquationComponents.getArgumentB());
+        assertEquals(operator, actualEquationComponents.getOperator());
+    }
+
+    @Test
+    void shouldReturnFalseForIncorrectArgumentsAndOperator() {
+        //Given
+        double argument1 = 8.9;
+        double argument2 = 1.2;
+        String operator = "-";
+        EquationComponents actualEquationComponents;
+
+        //When
+        actualEquationComponents = EntryProcessor.processEntry("9.8 + 1.3");
+
+        //Then
+        assertFalse(argument1 == actualEquationComponents.getArgumentA());
+        assertFalse(argument2 == actualEquationComponents.getArgumentB());
+        assertNotEquals(operator, actualEquationComponents.getOperator());
     }
 }
