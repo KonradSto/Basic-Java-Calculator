239ea5a5ff2736d252edd8ca031b08d20dd6acde
KonradSto <stolarczyk.konrad@outlook.com>
2019-05-08 21:53:44 +0200

    adds logic to calculations classes

--- src/main/java/calculations/Subtraction.java
+++ src/main/java/calculations/Subtraction.java
@@ -1,15 +1,15 @@
 package calculations;
 
-public class Division implements Calculation {
+public class Subtraction implements Calculation {
     private double a;
     private double b;
 
-    public Division(double a, double b) {
+    public Subtraction(double a, double b) {
         this.a = a;
         this.b = b;
     }
 
     public double calculate() {
-        return (a / b);
+        return (a - b);
     }
 }
